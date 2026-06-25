5b5585e839efd94e6fb54eb3f141999b849ede1d
Luiz1342 <luizotaviomagela@gmail.com>
2026-06-25 20:31:13 -0300

    Criada pasta src

---
+++ Estudos JS/6 Operadores Incremento e Decremento.js
@@ -0,0 +1,12 @@
+let contador = 1
+// '++' incremento cada ++ acrescenta 1
+contador++
+contador++
+
+// '--' decremento
+contador--
+contador--
+contador--
+
+console.log(contador)
+
---
+++ Estudos JS/8 Operadores Relacionais.js
@@ -0,0 +1,13 @@
+// Igual e diferente.
+// = é atribuição
+// == é para comparar o valor igual // != Diferente
+// === compara valor e formato // !== Formato e valor diferente
+
+let numero = "1"
+console.log(numero == 1)
+console.log(numero != 1)
+console.log(numero === 1)
+console.log(numero !== 1)
+
+let resultado = numero == 1 // Atribuindo à variavel resultado o valor da comparação. True ou False
+console.log(resultado)
